1b77ca062c26e664fa1867a7c77eeb2f8bec1185
Jason <Jason@MacBook-Pro-3.local>
2019-12-14 12:01:55 -0800

    MVP?

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -91,7 +91,7 @@ for (let i = 0; i < graduates.length;i++) {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter(grads => grads.tostring = 'Uni');
 console.log(unisWithUni);
 
 
